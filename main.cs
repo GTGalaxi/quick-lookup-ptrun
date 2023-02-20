7057982b29d845caa719f5fbd7c37531ce0d6855
GTGalaxi <gtgalaxiyt@gmail.com>
2023-02-20 14:50:54 +1100

    Created new Tool class, parsing method for tools.conf and moved the Tools into it for easy user configuration. Added Censys and Criminal IP to the list of default Tools.

--- main.cs
+++ main.cs
@@ -1,31 +1,78 @@
+using System;
+using System.IO;
 using ManagedCommon;
 using System.Collections.Generic;
 using Wox.Plugin;
+using System.Text.RegularExpressions;
 
 namespace QuickLookup
 {
+    public class Tool 
+    {
+        private string name;
+        private string url;
+        private bool enabled;
+
+        public Tool(string name, string url, bool enabled) {
+            this.name = name;
+            this.url = url;
+            this.enabled = enabled;
+        }
+
+        public string Name {
+            get { return name;}
+            set { name = value; }
+        }
+
+        public string URL {
+            get { return url;}
+            set { url = value; }
+        }
+
+        public bool Enabled {
+            get { return enabled;}
+            set { enabled = value; }
+        }
+        
+    }
+    public static class QLConfig 
+    {
+        public static List<Tool> ParseConfig() {
+            string config = File.ReadAllText("modules/launcher/Plugins/QuickLookup/tools.conf");
+            config = config.Replace("\r\n", "\n");
+            List<Tool> Tools  = Regex.Matches(config, @"(?:\w*=([^\n]*)\n?){3}", RegexOptions.Multiline)
+                                    .Cast<Match>()
+                                    .Select(tool => new Tool(tool.Groups[1].Captures[0].Value, tool.Groups[1].Captures[1].Value, tool.Groups[1].Captures[2].Value == "1" ? true : false))
+                                    .ToList();
+            return Tools;
+        }
+    }
     public class Main : IPlugin
     {
         private string ?IconPath { get; set; }
         private PluginInitContext ?Context { get; set; }
         public string Name => "Quick Lookup";
         public string Description => "Quick Lookup IP Addresses";
-        public List<string> Tools => new List<string>() { "https://www.shodan.io/host/", "https://viz.greynoise.io/ip/", "https://www.virustotal.com/gui/ip-address/"};
+        public List<Tool> Tools { get; set; }
+        public List<string> ToolsEnabled => new List<string>() { "" };
 
         public List<Result> Query(Query query)
         {
             var QueryIn = query.Search;
             List<Result> results = new List<Result>();
+            Tools = QLConfig.ParseConfig();
             results.Add(new Result
             {
                 Title = QueryIn,
-                SubTitle = "Quick Lookup in Shodan, GreyNoise & VirusTotal",
+                SubTitle = "Test",
                 IcoPath = IconPath,
                 Action = e =>
                 {
                     foreach (var Tool in Tools)
                     {
-                        System.Diagnostics.Process.Start("explorer", Tool+QueryIn);
+                        if (Tool.Enabled) {
+                            System.Diagnostics.Process.Start("explorer", string.Format(Tool.URL, QueryIn));
+                        }
                     }
                     return false;
                 }
@@ -38,6 +85,7 @@ namespace QuickLookup
             Context = context;
             Context.API.ThemeChanged += OnThemeChanged;
             UpdateIconPath(Context.API.GetCurrentTheme());
+            Tools = QLConfig.ParseConfig();
         }
 
         private void UpdateIconPath(Theme theme)
