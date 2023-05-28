87f6e789d2d5f4e9d568a463b95cf020a9762d1d
Mitch Smith <gtgalaxiyt@gmail.com>
2023-05-28 17:27:13 +1000

    feat: update to version 1.3.0!
    Update config to use JSON and implement tab autocomplete for categories!
    
    Change config file and parsing from custom implementation to JSON
    Update category filter returns to allow for tab autocomplete
    Create function to convert old tools.conf to new tools.json
    Create new Tools class to replace use of List<Tool>
    Create helper functions for new Tools class
    Refator codebase to improve readability, functionality and ease of use
    Update README.md documentation to reflect changes

--- main.cs
+++ main.cs
@@ -12,169 +12,128 @@ namespace QuickLookup
         private string url;
         private string[] categories;
         private bool enabled;
-
         public Tool(string name, string url, string[] categories, bool enabled) {
             this.name = name;
             this.url = url;
             this.categories = categories;
             this.enabled = enabled;
         }
-
-        public string Name {
-            get { return name;}
-            set { name = value; }
-        }
-
-        public string URL {
-            get { return url;}
-            set { url = value; }
-        }
-
-        public string[] Categories {
-            get { return categories;}
-            set { categories = value; }
-        }
-
-        public bool Enabled {
-            get { return enabled;}
-            set { enabled = value; }
-        }
+        public string Name { get { return name;} set { name = value; } }
+        public string URL { get { return url;} set { url = value; } } 
+        public string[] Categories { get { return categories;} set { categories = value; } } 
+        public bool Enabled { get { return enabled;} set { enabled = value; } }
         
+    }
+    public class Tools
+    {
+        private List<Tool> tools { get; set; }
+        public Tools() { this.tools = QLConfig.ParseConfig(); }
+        public Tools(List<Tool> tools) { this.tools = tools; }
+        public Tools Update() { return this.tools = QLConfig.ParseConfig(); }
+        public IEnumerable<Tool> GetEnumerator() { return this.tools; }
+        public static implicit operator List<Tool>(Tools t) => t.tools;
+        public static implicit operator Tools(List<Tool> t) => new Tools(t);
+        public static Tools DefaultTools() {
+            return new List<Tool> {
+                new Tool("Shodan","https://www.shodan.io/host/{0}",new string[] {"ip"},true),
+                new Tool("VirusTotal","https://www.virustotal.com/gui/search/{0}",new string[] {"ip","domain","hash"},true)
+            };
+        }
     }
     public static class QLConfig 
     {
-        public static List<Tool>? ParseConfig(bool force = false) {
-            List<Tool>? Tools;
-            if (File.Exists("modules/launcher/Plugins/QuickLookup/tools.json") && force == false) {
-                try {
-                    string config = File.ReadAllText("modules/launcher/Plugins/QuickLookup/tools.json");
-                    Tools = JsonSerializer.Deserialize<List<Tool>>(config);
-                } catch {
-                    Tools = null;
-                }
-            } else if (File.Exists("modules/launcher/Plugins/QuickLookup/tools.conf")) {
-                string config = File.ReadAllText("modules/launcher/Plugins/QuickLookup/tools.conf");
-                config = config.Replace("\r\n", "\n");
-                Tools  = Regex.Matches(config, @"(?:\w*=([^\n]*)\n?){4}", RegexOptions.Multiline)
-                                        .Cast<Match>()
-                                        .Select(tool => new Tool(tool.Groups[1].Captures[0].Value, tool.Groups[1].Captures[1].Value.Replace("\"", ""), tool.Groups[1].Captures[2].Value.ToLower().Split(","), tool.Groups[1].Captures[3].Value == "1" ? true : false))
-                                        .ToList();
-            } else {
-                Tools = null;
-            }
-            return Tools;
-        }
-
-        public static bool ConvertConfigToJSON() {
-            // If tools.json exists, do not convert. Already converted or custom config in place.
-            if (File.Exists("modules/launcher/Plugins/QuickLookup/tools.json")) {
-                return true;
-            }
-            // Parse tools.conf and convert to JSON
-            List<Tool>? Tools = ParseConfig(true);
-            if (Tools is null) {
-                return false;
-            } 
-
+        public static Tools ParseConfig(bool force = false) {
+            Tools tools = Tools.DefaultTools();
+            string pluginDir = Main.Context.CurrentPluginMetadata.PluginDirectory;
             try {
-                string jsonString = JsonSerializer.Serialize(Tools, new JsonSerializerOptions { WriteIndented = true });
-                File.WriteAllText("modules/launcher/Plugins/QuickLookup/tools.json", Regex.Unescape(jsonString));
-            } catch {
-                return false;
-            }
-            return true;
-        }
-        
-        public static string UpdateSubTitle(List<Tool> Tools, string Category) {
-            string subTitle = string.Join(", ", Tools.Where(t => t.Categories.Contains(Category)).Where(t => t.Enabled).Select(t => t.Name));
-            if (subTitle == "") {
-                return "No tools found in category!";
-            }
-            if (!string.IsNullOrEmpty(subTitle))
-            {
-                int place = subTitle.LastIndexOf(", ");
-                if (place >= 0)
-                {
-                    subTitle = subTitle.Remove(place, 2).Insert(place, " & ");
+                if (File.Exists(pluginDir+"\\tools.json") && force == false) {
+                        string config = File.ReadAllText(pluginDir+"\\tools.json");
+                        tools = JsonSerializer.Deserialize<List<Tool>>(config) ?? Tools.DefaultTools();
+                } else if (File.Exists(pluginDir+"\\tools.conf")) {
+                        string config = File.ReadAllText(pluginDir+"\\tools.conf");
+                        config = config.Replace("\r\n", "\n");
+                        tools  = (Tools)Regex.Matches(config, @"(?:\w*=([^\n]*)\n?){4}", RegexOptions.Multiline).Cast<Match>()
+                            .Select(tool => {var cTool = tool.Groups[1].Captures; return new Tool(cTool[0].Value, cTool[1].Value.Replace("\"", ""), cTool[2].Value.ToLower().Split(","), cTool[3].Value == "1" ? true : false);});
                 }
-            }
-            return subTitle;
+            } catch(Exception e) { Wox.Plugin.Logger.Log.Exception(e.Message, e, typeof(QLConfig), "ParseConfig"); tools = Tools.DefaultTools(); }
+            return tools;
         }
-        public static string FilterCategories(List<Tool> Tools, string Category) {
-            string subTitle = string.Join(", ", GetCategories(Tools).Where(c => (c.Contains(Category.ToLower()) | Category == ""))).ToUpper();
-            if (subTitle == "") {
-                return "No matching Categories!";
-            }
-            if (!string.IsNullOrEmpty(subTitle))
-            {
-                int place = subTitle.LastIndexOf(", ");
-                if (place >= 0)
-                {
-                    subTitle = subTitle.Remove(place, 2).Insert(place, " & ");
-                }
-            }
-            return subTitle;
+        public static bool ConvertConfigToJSON(this Tools Tools, bool force = false) {
+            string pluginDir = Main.Context.CurrentPluginMetadata.PluginDirectory;
+            if (File.Exists(pluginDir+"\\tools.json") && force == false) { return true; }
+            try {
+                string jsonString = JsonSerializer.Serialize(Tools.GetEnumerator(), new JsonSerializerOptions { WriteIndented = true });
+                File.WriteAllText(pluginDir+"\\tools.json", Regex.Unescape(jsonString));
+                return true;
+            } catch { return false; }
         }
-        
-        public static string[] GetCategories(List<Tool> Tools) {
-            List<string> categories = new List<string>();
-            string[][] t = Tools.Select(t => t.Categories).ToArray();
-            foreach (var cats in t) {
-                foreach (var cat in cats) {
-                    if (!categories.Contains(cat)) {
-                        categories.Add(cat);
-                    }
-                }
-            };
-            return categories.ToArray();
+        public static string[] FilterTools(List<Tool> Tools, string Category) {
+            return Tools.Where(t => t.Categories.Contains(Category)).Where(t => t.Enabled).Select(t => t.Name).ToArray();
+        }
+        public static string[] FilterCategories(List<Tool> Tools, string PartialCategory) {
+            return Tools.SelectMany(t => t.Categories).Distinct().Where(c => (c.Contains(PartialCategory.ToLower()) | PartialCategory == "")).ToArray();
+        }
+        public static string Subtitle(this string[] items) {
+            return items.Length >= 1 ? (items.Length >= 2 ? string.Join(", ", items[..(items.Length-1)]) + " & " + items[(items.Length-1)] : items[0]) : "No matching Categories or no Tools found in Category!";
+        }
+        public static (string, int) Subtitle(this string[] items, bool includeCount = true) {
+            return (items.Subtitle(), items.Count());
         }
     }
     public class Main : IPlugin
     {
-        private string? IconPath { get; set; }
-        private PluginInitContext? Context { get; set; }
+        private string IconPath { get; set; } = "img/ql.dark.png";
+        public static PluginInitContext Context { get; set; } = null!;
         public string Name => "Quick Lookup";
         public string Description => "Quick Lookup";
         public string SubTitle = "Quick Lookup";
-        public List<Tool>? Tools { get; set; }
+        public Tools ToolList { get; set; } = null!;
 
         public List<Result> Query(Query query)
         {
-            Tools = QLConfig.ParseConfig();
             List<Result> results = new List<Result>();
-            
-            if (Tools is null) {
-                results.Add(new Result
-                {
+            ToolList.Update();
+            if (Context is null) {
+                results.Add(new Result {
                     Title = "ERROR",
-                    SubTitle = "Error Parsing Tools or no Tools found!",
+                    SubTitle = "Error: null reference to Context",
                     IcoPath = IconPath,
-                    Action = e =>
-                    {
-                        return false;
-                    }
+                    Action = e => { return false; }
                 });
                 return results;
             }
-            
-            var QueryIn = query.Search;
-            var QuerySplit = QueryIn.Split(" ");
-            if (QuerySplit.Length >= 2) {
-                var Category = QuerySplit[0].ToLower();
-                var Input = QuerySplit[1];
-
-                SubTitle = "Quick Lookup "+ Category.ToUpper() + " using: " + QLConfig.UpdateSubTitle(Tools, Category);
+            if (ToolList is null) {
                 results.Add(new Result
                 {
-                    Title = Input,
+                    Title = "ERROR",
+                    SubTitle = "Error Parsing Tools or no Tools found! Press ENTER to open Plugin folder",
+                    IcoPath = IconPath,
+                    Action = e => { Process.Start("explorer", Context.CurrentPluginMetadata.PluginDirectory); return false; }
+                });
+                return results;
+            }
+            var QuerySplit = query.Search.Split(" ");
+            string category = QuerySplit[0].ToLower();
+            (string categories, int count) = QLConfig.FilterCategories(ToolList, category).Subtitle(true);
+            if (count == 1 && category == categories.ToLower()) {
+                string input;
+                if (QuerySplit.Length <= 1) {
+                    input = "";
+                } else {
+                    input = QuerySplit[1];
+                }
+                SubTitle = "Quick Lookup "+ category.ToLower() + " using: " + QLConfig.FilterTools(ToolList, category).Subtitle();
+                results.Add(new Result
+                {
+                    Title = input,
                     SubTitle = SubTitle,
                     IcoPath = IconPath,
                     Action = e =>
                     {
-                        foreach (var Tool in Tools.Where(t => t.Categories.Contains(Category)))
+                        foreach (var Tool in ToolList.GetEnumerator().Where(t => t.Categories.Contains(category)))
                         {
                             if (Tool.Enabled) {
-                                Process.Start(new ProcessStartInfo(string.Format(Tool.URL, Input)) { UseShellExecute = true });
+                                Process.Start(new ProcessStartInfo(string.Format(Tool.URL, input)) { UseShellExecute = true });
                             }
                         }
                         return false;
@@ -182,10 +141,10 @@ namespace QuickLookup
                 });
             }
             else if (QuerySplit.Length <= 1) {
-                SubTitle = "Categories: " + QLConfig.FilterCategories(Tools, QueryIn);
+                SubTitle = "Categories: " + categories;
                 results.Add(new Result
                 {
-                    Title = QueryIn,
+                    Title = count == 1 ? categories.ToLower() : query.Search,
                     SubTitle = SubTitle,
                     IcoPath = IconPath,
                     Action = e =>
@@ -196,22 +155,15 @@ namespace QuickLookup
             }
             return results;
         }
-
         public void Init(PluginInitContext context)
         {
             Context = context;
             Context.API.ThemeChanged += OnThemeChanged;
             UpdateIconPath(Context.API.GetCurrentTheme());
-            QLConfig.ConvertConfigToJSON();
-            Tools = QLConfig.ParseConfig();
+            ToolList = new Tools();
+            ToolList.ConvertConfigToJSON();
         }
-
-        private void UpdateIconPath(Theme theme)
-        {
-            if (theme == Theme.Light || theme == Theme.HighContrastWhite) { IconPath = "img/ql.light.png"; } // Light Theme 
-            else { IconPath = "img/ql.dark.png"; } // Dark Theme 
-        }
-
+        private void UpdateIconPath(Theme theme) { IconPath = (theme == Theme.Light || theme == Theme.HighContrastWhite) ? "img/ql.light.png" : "img/ql.dark.png"; }
         private void OnThemeChanged(Theme currentTheme, Theme newTheme) { UpdateIconPath(newTheme); }
     }
 }
